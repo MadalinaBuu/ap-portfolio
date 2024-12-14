53a008e30d7a7479d378a2fcef9f030eacbe90a1
MadalinaBuu <madalina.ciucioiu@gmail.com>
2024-12-14 15:12:03 +0200

    added navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  menuItems = [
+    { label: 'Acasa', path: '#' },
+    { label: 'Despre Mine', path: '#' },
+    { label: 'Portofoliu', path: '#' },
+    { label: 'Testimoniale', path: '#' },
+    { label: 'Contact', path: '#' },
+    { label: 'Q&A', path: '#' }
+  ];
 }
